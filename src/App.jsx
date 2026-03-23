0293db0db8f899fd7cf8c0c45639d312ac570ca0
Ankita <ankitaharihar81@gmail.com>
2026-03-23 07:41:46 +0530

    feat: add routing with react-router-dom and implement HeroSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header'
+import HeroSection from './Components/HeroSection'
 
 
 function App() {
   return (
-    <>
-    <Header></Header>
-    </>
+    <HeroSection />
   )
     
 }
